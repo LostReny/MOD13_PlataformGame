56915526d6a88ab030f87404fb93135440d546de
Isabelle Reny <isabelleribaldo@hotmail.com>
2023-11-20 15:23:44 -0300

    life_collect

--- Assets/Scripts/Health/HealthManager.cs
+++ Assets/Scripts/Health/HealthManager.cs
@@ -4,40 +4,7 @@ using UnityEngine;
 using Singleton;
 using System.Drawing;
 
-public class HealthManager : HealthBase
+public class HealthManager : ItemCollectBase
 {
-    /*
-    public SOInt playerLife;
 
-    private void Awake()
-    {
-        if (Instance == null)
-            Instance = this;
-        else
-            Destroy(gameObject);
-    }
-
-    private void Update()
-    {
-
-    }
-
-    private void Start()
-    {
-        Reset();
-    }
-
-    private void Reset()
-    {
-        
-
-    }
-
-    public void ChangeLife(int life)
-    {
-        life = _currentLife;
-
-        playerLife = life.value.ToString();    
-    }
-    */
 }
